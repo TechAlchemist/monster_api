d183c10f36d9664d281c79abc7f8a2321daf7aa6
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-08 14:04:21 -0800

    Ensuring seed file works.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 Monster.create([
     {name: 'Bigfoot', height: 8, weight: 300, description: "Tall, covered in hair, color varies based on environment, large feet, ape like, and also goes by Yeti, Sasquatch, Bumble, and Abominable Snowman."},
