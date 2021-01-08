22c26dbc9401111b5654650073f0237d791f3c35
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-08 14:37:26 -0800

    updating origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://monster-client-frontend.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
